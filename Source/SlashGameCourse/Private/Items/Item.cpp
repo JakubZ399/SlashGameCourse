de697994bdebc1ea6dcaf97c7bdca4b80d079964
kubaz <zurawikjakub@gmail.com>
2024-10-08 17:40:20 +0200

    Importing Animations From Mixamo

--- Source/SlashGameCourse/Private/Items/Item.cpp
+++ Source/SlashGameCourse/Private/Items/Item.cpp
@@ -26,21 +26,13 @@ void AItem::BeginPlay()
 void AItem::OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
 	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
-	 const FString OtherActorName = OtherActor->GetName();
 
-	if (GEngine)
-	{
-		GEngine->AddOnScreenDebugMessage(1, 30.f, FColor::Red, OtherActorName);
-	}
 }
 
 void AItem::OnSphereEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
 	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
 {
-	if (GEngine)
-	{
-		GEngine->AddOnScreenDebugMessage(1, 30.f, FColor::Purple, OtherActor->GetName());
-	}
+
 }
 
 void AItem::Tick(float DeltaTime)
